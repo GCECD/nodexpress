888e7721281196d55bc30bcf5ae1886ce2481977
GCECD <nadkarnipallavi2@gmail.com>
2017-05-15 14:46:53 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello to the World.... from India!");
+  response.end("Hello to the World.!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
